3931136d165dcbe6fa5f0a16b09f5e4d9703ed6c
JaumeFullana <74202163+JaumeFullana@users.noreply.github.com>
2020-11-30 22:24:37 +0100

    retoques, bucle y menu

--- MenuJuego.py
+++ MenuJuego.py
@@ -5,8 +5,7 @@ import funciones
 
 import objetos
 
-#Hacer bucle de partida i que el jugador 1 vaya rotando en cada ronda.
-#Hacer menu del juego donde puedas salir de la partida, añadir jugador o añadir dinero despues de cada partida
+#Hacer que el jugador 1 vaya rotando en cada ronda.
 #Hacer lista de la banca [Dinero,[cartas],valorcartas] y sus funciones
 
 print ("╔════════════════════════════════════════════════════════════════════╗")
@@ -26,84 +25,139 @@ listajugadores=funciones.nombrejugadores(numerojugadores)
 print("\n>>> Con cuanto dinero vais a entrar cada uno?")
 
 funciones.dinerojugadores(listajugadores)
-os.system('cls')
 
-print("╔════════════════════════════════════════════════════════════════════════╗")
-print("║                                                                        ║")
-print("║                        ¡Que empiece la partida!                        ║")
-print("║                                                                        ║")
-print("╚════════════════════════════════════════════════════════════════════════╝")
+opcion=""
 
-print ("\n>>> Hagan sus apuestas!\n")
+while opcion!="terminar":
 
-funciones.apuestainicialjugadores(listajugadores)
-
-baraja=funciones.barajar(objetos.baraja)
-
-funciones.repartircartasiniciales(listajugadores,baraja)
-
-#Revelar carta
-
-funciones.repartircartasiniciales(listajugadores,baraja)
-
-for i in range (len(listajugadores)):
     os.system('cls')
-    funciones.valorcartas(listajugadores[i])
-    funciones.vercartas(listajugadores,listajugadores[i][0])
-    respuesta=input(f">>> Quieres una carta mas {listajugadores[i][0]}?\n >  ")
 
-    while respuesta not in ["si","no"]:
+    print("╔════════════════════════════════════════════════════════════════════════╗")
+    print("║                                                                        ║")
+    print("║                        ¡Que empiece la partida!                        ║")
+    print("║                                                                        ║")
+    print("╚════════════════════════════════════════════════════════════════════════╝")
 
-        print("\n ⚠  Porfavor escriba si o no")
+    print ("\n>>> Hagan sus apuestas!\n")
+
+    funciones.apuestainicialjugadores(listajugadores)
+
+    baraja=funciones.barajar(objetos.baraja)
+
+    funciones.repartircartasiniciales(listajugadores,baraja)
+
+    funciones.repartircartasiniciales(listajugadores,baraja)
+
+    for i in range (len(listajugadores)):
+        os.system('cls')
+        funciones.valorcartas(listajugadores[i])
+        funciones.vercartas(listajugadores,listajugadores[i][0])
         respuesta=input(f">>> Quieres una carta mas {listajugadores[i][0]}?\n >  ")
 
-    vecesdoblado=0
+        while respuesta not in ["si","no"]:
 
-    while respuesta==("si"):
+            print("\n ⚠  Porfavor escriba si o no")
+            respuesta=input(f">>> Quieres una carta mas {listajugadores[i][0]}?\n >  ")
 
-        while vecesdoblado==0:
+        vecesdoblado=0
 
-            funciones.apuestajugadores(listajugadores,i)
-            os.system('cls')
+        while respuesta==("si"):
 
-            vecesdoblado=1
-        
-        #Revelar carta tapada
+            while vecesdoblado==0:
 
-        funciones.repartircarta(listajugadores,baraja,i)
+                funciones.apuestajugadores(listajugadores,i)
+                os.system('cls')
 
-        funciones.vercartas(listajugadores,listajugadores[i][0])
+                vecesdoblado=1
 
-        pasado=funciones.valorcartas(listajugadores[i])
+            funciones.repartircarta(listajugadores,baraja,i)
 
-        if not pasado:
+            funciones.vercartas(listajugadores,listajugadores[i][0])
 
-            respuesta=input(">>> Quieres una carta mas?\n >  ")
+            pasado=funciones.valorcartas(listajugadores[i])
 
-            while respuesta not in ["si","no"]:
+            if not pasado:
 
-                print("\n ⚠  Porfavor escriba si o no")
                 respuesta=input(">>> Quieres una carta mas?\n >  ")
-            os.system('cls')
 
-        if pasado:
+                while respuesta not in ["si","no"]:
 
-            respuesta="no"
+                    print("\n ⚠  Porfavor escriba si o no")
+                    respuesta=input(">>> Quieres una carta mas?\n >  ")
+                os.system('cls')
 
-    
+            if pasado:
 
-    os.system('cls')
+                respuesta="no"
 
-    if i != len(listajugadores)-1:
-        print("╔════════════════════════════════════════════════════════════════════╗")
-        print("║                                                                    ║")
-        print("║                 Vale! pasemos al siguiente jugador                 ║")
-        print("║                                                                    ║")
-        print("╚════════════════════════════════════════════════════════════════════╝")
         
-        input(">>> Pulsa ENTER para pasar al siguiente jugador.\n >  ")
 
-funciones.vermesa(listajugadores)
+        os.system('cls')
+
+        if i != len(listajugadores)-1:
+            print("╔════════════════════════════════════════════════════════════════════╗")
+            print("║                                                                    ║")
+            print("║                 Vale! pasemos al siguiente jugador                 ║")
+            print("║                                                                    ║")
+            print("╚════════════════════════════════════════════════════════════════════╝")
+            
+            input(">>> Pulsa ENTER para pasar al siguiente jugador.\n >  ")
+
+    funciones.vermesa(listajugadores)
+
+    print ("MENU DEL JUEGO")
+
+    print ("Aqui cada jugador puede salir de la partida o añadir dinero!\n\
+Ademas pueden entrar a jugar mas personas mientras se respete el numero maximo de jugadores.")
+
+    for i in range(len(listajugadores)):
+
+        opcion=input(f"{listajugadores[i][0]} Escribe salir si quieres salir de la partida, añadir si quieres añadir dinero o \
+pulsa cualquier otra cosa para seguir jugando asi:")
+
+        if opcion=="salir":
+
+            print (f"Vale! Hasta la proxima {listajugadores[i][0]}!")
+
+            del listajugadores[i]
+
+        elif opcion=="añadir":
+
+            dinero=int(input("Cuanto dinero quieres añadir?"))
+
+            dinerototal=listajugadores[i][1]+dinero
+
+            del listajugadores[i][1]
+
+            listajugadores[i].insert(1,dinerototal)
+
+        else:
+
+            ("Seguimos asi pues!")
+
+    if (len(listajugadores))<7:
+
+        masjugadores=input("Van a entrar a jugar mas jugadores?")
+
+        if masjugadores=="si":
+
+            numeroNuevosJugadores=int(input("Cuantos jugadores se van a añadir?"))
+
+            while (len(listajugadores))+numeroNuevosJugadores>7:
+
+                numeroNuevosJugadores=int(input("No se pueden añadir tantos jugadores! recordad que el maximo son 7!"))
+
+            #Nombre jugador
+
+            #Dinero jugador
+
+        if masjugadores=="no":
+
+            ("Sigamos pues!")
+
+    print (listajugadores)
+
+
 
 print (listajugadores)
 
--- Pruebas.py
+++ Pruebas.py
@@ -2,12 +2,4 @@
 
 lista=["jugador","sudinero",["apuesta"],["cartasenMano"],"valormano"]
 
-apuesta=lista[2][0]
-
-doblar=apuesta+apuesta
-
-lista.pop([2][0])
-
-lista.insert(2,[doblar])
-
-print(lista)
+print(lista[-2:])
