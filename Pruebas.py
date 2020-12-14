4c2f0e6dafa16bc91a19f462309624ce36d3452a
JLCardona <jcardona@cifpfbmoll.eu>
2020-12-14 18:12:11 +0100

    commit

--- Pruebas.py
+++ Pruebas.py
@@ -1,9 +1,39 @@
-lista=["jugador","sudinero","apuesta",["cartasenMano"],"valormano"]
+def valorcartas (listajugadores,listabanca):
+    valormano=0
+    apuesta=listajugadores[2]
+    for i in listajugadores[3]:
+        valormano+=objetos.valor_baraja.get(i)
+    while valormano>21:
+        
+    if valormano>21:
+        if listajugadores[-1]==listajugadores[3]:
+            listajugadores.append(valormano)
+            print ("   >>> Looseeeeer!")
+            print ("   >>> Tu puntuación es mayor a 21.")
+            dinerojugador=(listajugadores.pop(1))-apuesta
+            listajugadores.insert(1,dinerojugador)
+            dinerobanca=listabanca.pop(0)+apuesta
+            listabanca.insert(0,dinerobanca)
+            pasado=True
+            prints.colorinput("\n   >>> Pulsa ENTER para abandonar la mesa.")
+        else:
+            del listajugadores[4]
+            listajugadores.append(valormano)
+            print ("   >>> Looseeeeer!")
+            print ("   >>> Tu puntuación es mayor a 21.")
+            dinerojugador=(listajugadores.pop(1))-apuesta
+            listajugadores.insert(1,dinerojugador)
+            dinerobanca=listabanca.pop(0)+apuesta
+            listabanca.insert(0,dinerobanca)
+            pasado=True
+            prints.colorinput("\n   >>> Pulsa ENTER para abandonar la mesa.")
+    else:
+        if listajugadores[-1]==listajugadores[3]:
+            listajugadores.append(valormano)
+            pasado=False
+        else:
+            del listajugadores[4]
+            listajugadores.append(valormano)
+            pasado=False
 
-listabanca=["Dinero",["cartas"],"valorcartas"]
-
-import prints
-
-prints.adios()
-
-input("")
+    return pasado
